3a8200c9951f89ba8227bedf24776f468773beb3
Crosby <xxristuaji141@gmail.com>
2023-01-13 00:30:55 +0700

    Commit ke github
    commit pertama dan upload ke github

---
+++ ESP 32 RELAY CONTROL FIREBASE.ino
@@ -0,0 +1,62 @@
+#include "FirebaseESP32.h"
+#include <WiFi.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+const char* ssid = "ScnPh";
+const char* password = "kanudahnyambung";
+
+FirebaseData firebaseData;
+FirebaseJson json;
+
+#define relay 16
+#define LED 23
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+  pinMode(LED, OUTPUT);
+  digitalWrite(relay, LOW); //nilai awal relay akan off 
+  
+  konekWifi();
+  Firebase.begin("https://esp32-relaycontrol-firebase-default-rtdb.firebaseio.com", "ybELyOWqAeqZgHj5sS4uZF3iHSWvOOUEr61fCeKx");
+  //Firebase.begin("firebase host", "firebase auth database");
+}
+
+void konekWifi() {
+  WiFi.begin(ssid, password);
+  //memulai menghubungkan ke wifi router
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("."); //status saat mengkoneksikan
+  }
+  Serial.println("Sukses terkoneksi wifi!");
+  Serial.println("IP Address:"); //alamat ip lokal
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if (Firebase.getString(firebaseData, "/RelayControl/nilai")) { //misal database diberikan nama relay1
+    if  (firebaseData.dataType() == "string") 
+    {
+      String FBStatus = firebaseData.stringData();
+      if (FBStatus == "1") {                                                         
+        Serial.println("Relay ON");                         
+        Serial.print("Pompa ON");                         
+        digitalWrite(relay, HIGH);
+        digitalWrite(LED, HIGH);
+        Firebase.RTDB.setString(&firebaseData, "/RelayControl/status", "Pompa_Hidup");
+
+        }else if (FBStatus == "0") {                                                  
+        Serial.println("Relay OFF");
+        Serial.print("Pompa OFF");                         
+        digitalWrite(relay, LOW);
+        digitalWrite(LED, LOW);
+        Firebase.RTDB.setString(&firebaseData,  "/RelayControl/status", "Pompa_Mati");
+
+        }else {
+        Serial.println("Salah kode! isi dengan data ON/OFF");
+        }
+    }
+  }
+}
