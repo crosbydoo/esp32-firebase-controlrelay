7d5c56ebe951ba6b194873ed3cab0ef88e5a42eb
Crosby <xxristuaji141@gmail.com>
2023-01-13 00:36:18 +0700

    update minor bug
    memperbaiki dan merestrict kode yang penting dan menghapus token sendiri. agar tidak disalah gunakan

--- ESP 32 RELAY CONTROL FIREBASE.ino
+++ ESP 32 RELAY CONTROL FIREBASE.ino
@@ -3,8 +3,8 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-const char* ssid = "ScnPh";
-const char* password = "kanudahnyambung";
+const char *ssid = "USERNAME WIFI";
+const char *password = "PASSWORD WIFI ";
 
 FirebaseData firebaseData;
 FirebaseJson json;
@@ -12,51 +12,59 @@ FirebaseJson json;
 #define relay 16
 #define LED 23
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(relay, OUTPUT);
   pinMode(LED, OUTPUT);
-  digitalWrite(relay, LOW); //nilai awal relay akan off 
-  
+  digitalWrite(relay, LOW); // nilai awal relay akan off
+
   konekWifi();
-  Firebase.begin("https://esp32-relaycontrol-firebase-default-rtdb.firebaseio.com", "ybELyOWqAeqZgHj5sS4uZF3iHSWvOOUEr61fCeKx");
-  //Firebase.begin("firebase host", "firebase auth database");
+  Firebase.begin("LINK REALTIME DATABASE", "SECRET KODE DATABASE FIREBASE");
+  // Firebase.begin("firebase host", "firebase auth database");
 }
 
-void konekWifi() {
+void konekWifi()
+{
   WiFi.begin(ssid, password);
-  //memulai menghubungkan ke wifi router
-  while (WiFi.status() != WL_CONNECTED) {
+  // memulai menghubungkan ke wifi router
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
-    Serial.print("."); //status saat mengkoneksikan
+    Serial.print("."); // status saat mengkoneksikan
   }
   Serial.println("Sukses terkoneksi wifi!");
-  Serial.println("IP Address:"); //alamat ip lokal
+  Serial.println("IP Address:"); // alamat ip lokal
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  if (Firebase.getString(firebaseData, "/RelayControl/nilai")) { //misal database diberikan nama relay1
-    if  (firebaseData.dataType() == "string") 
+void loop()
+{
+  if (Firebase.getString(firebaseData, "/RelayControl/nilai"))
+  { // misal database diberikan nama relay1
+    if (firebaseData.dataType() == "string")
     {
       String FBStatus = firebaseData.stringData();
-      if (FBStatus == "1") {                                                         
-        Serial.println("Relay ON");                         
-        Serial.print("Pompa ON");                         
+      if (FBStatus == "1")
+      {
+        Serial.println("Relay ON");
+        Serial.print("Pompa ON");
         digitalWrite(relay, HIGH);
         digitalWrite(LED, HIGH);
         Firebase.RTDB.setString(&firebaseData, "/RelayControl/status", "Pompa_Hidup");
-
-        }else if (FBStatus == "0") {                                                  
+      }
+      else if (FBStatus == "0")
+      {
         Serial.println("Relay OFF");
-        Serial.print("Pompa OFF");                         
+        Serial.print("Pompa OFF");
         digitalWrite(relay, LOW);
         digitalWrite(LED, LOW);
-        Firebase.RTDB.setString(&firebaseData,  "/RelayControl/status", "Pompa_Mati");
-
-        }else {
+        Firebase.RTDB.setString(&firebaseData, "/RelayControl/status", "Pompa_Mati");
+      }
+      else
+      {
         Serial.println("Salah kode! isi dengan data ON/OFF");
-        }
+      }
     }
   }
 }
